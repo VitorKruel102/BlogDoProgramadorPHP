9f5191892a2b6fee6d65f0e1314c62e16f2c30cb
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-11-14 15:05:01 -0300

    up.

--- application/views/Home/home.php
+++ application/views/Home/home.php
@@ -6,12 +6,12 @@
             <h1 class="page-header">
                 Página Inicial - <small>Postagens Recentes</small>
             </h1>
-
-
-            <!-- First Blog Post -->
+            <!-- Posts -->
             <?php foreach($posts as $post): ?>
                 <h2>
-                    <a href="#"><?= $post->titulo ?></a>
+                    <a href="<?= base_url("postagem/$post->id/".snake_case($post->titulo)) ?>">
+                        <?= $post->titulo ?>
+                    </a>
                 </h2>
                 <p class="lead">
                     por <a href="index.php"> Start Bootstrap</a>
@@ -26,7 +26,7 @@
                 <p>
                     <?= $post->conteudo ?>
                 </p>
-                <a class="btn btn-primary" href="#">
+                <a class="btn btn-primary" href="<?= base_url("postagem/$post->id/".snake_case($post->titulo)) ?>">
                     Leia mais <span class="glyphicon glyphicon-chevron-right"></span>
                 </a>
                 <hr>
