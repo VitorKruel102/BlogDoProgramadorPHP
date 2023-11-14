9c9541d0cf182bce8b722e608db8e813e57723e6
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-11-14 10:46:53 -0300

    Organizando projeto Home.

---
+++ application/views/Home/templates/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Website design for a programmer's blog in PHP">
+        <meta name="author" content="Vitor Kruel">
+
+        <title>Blog Home</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="<?=base_url('assests/Home/css/bootstrap.min.css')?>" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="<?=base_url('assests/Home/css/blog.min.css')?>" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link 
+            href="<?=base_url('assests/Home/css/font-awesome/css/font-awesome.min.css')?>" 
+            rel="stylesheet" 
+            type="text/css"
+        >
+    </head>
+    <body>
---
+++ application/views/Home/templates/html-footer.php
@@ -0,0 +1,7 @@
+        <!-- jQuery -->
+        <script src="<?=base_url('assests/Home/js/jquery.js')?>"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="<?=base_url('assests/Home/js/bootstrap.min.js')?>"></script>
+    </body>
+</html>
