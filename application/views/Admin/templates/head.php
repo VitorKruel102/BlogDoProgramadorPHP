b7d278b55a1d363adf8a364e39608fb040aafc13
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-11-14 12:17:26 -0300

    Ajustando a estrutura de arquivos da área administrativa(Admin);

---
+++ application/views/Admin/templates/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Website design for a programmer's blog in PHP">
+        <meta name="author" content="Vitor Kruel">
+
+        <title>Painel Administrativo</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="<?=base_url('assests/Admin/css/bootstrap.min.css')?>" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="<?=base_url('assests/Admin/css/sb-admin-2.min.css')?>" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link 
+            href="<?=base_url('assests/Admin/css/font-awesome/css/font-awesome.min.css')?>" 
+            rel="stylesheet" 
+            type="text/css"
+        >
+    </head>
+    <body>
---
+++ application/views/Admin/templates/html-footer.php
@@ -0,0 +1,12 @@
+        </div>
+        <!-- /#wrapper -->
+        <!-- jQuery -->
+        <script src="<?=base_url('assests/Admin/js/jquery.min.js')?>"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="<?=base_url('assests/Admin/js/bootstrap.min.js')?>"></script>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="<?=base_url('assests/Admin/js/sb-admin-2.js')?>"></script>
+    </body>
+</html>
